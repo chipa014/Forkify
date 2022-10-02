84e6686d3effd42dfd15b232616b19cb53170adc
chipa014 <alextheracoonthesecond@gmail.com>
2022-10-02 21:21:10 +0700

    Fixing fractional/parcel build conflict

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const DEV_KEY = '66a4b4cc-59f4-42ea-b3e1-2d9a79cf0d42';
+export const DEV_KEY = '493db6f6-70d5-4bf3-b50f-2156f93c3162';
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
 export const MODAL_CLOSE_TIME = 1500;
