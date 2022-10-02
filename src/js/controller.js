32c1c2ff69830b6f75845bc4884800f63de166bc
chipa014 <alextheracoonthesecond@gmail.com>
2022-10-02 19:19:24 +0700

    Minor cleanup

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,10 +15,6 @@ const recipeContainer = document.querySelector('.recipe');
 
 ///////////////////////////////////////
 
-if (module.hot) {
-  module.hot.accept();
-}
-
 const controlRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
